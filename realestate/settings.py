86e400cd528d2287c891a4cb662d5e7c8945ac0e
vishnupv863 <vishnupv863@gmail.com>
2025-06-26 15:38:35 +0530

    Ready for deployment to Render

--- realestate/settings.py
+++ realestate/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = ["*"]  # For testing; later replace with Render’s domain
+STATIC_ROOT = BASE_DIR / "staticfiles"
