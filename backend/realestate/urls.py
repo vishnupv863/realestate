b2d9a9593da892e0c7f401f43fa623a792595872
vishnupv863 <vishnupv863@gmail.com>
2025-07-17 23:24:19 +0530

    api added in urls

--- backend/realestate/urls.py
+++ backend/realestate/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("properties.urls")),
-    path("auth/", include("accounts.urls")),
+    path("api/auth/", include("accounts.urls")),
 ]
