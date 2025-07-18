2669e48bba6270daa97a46cf2b72c6f380f4f34c
vishnupv863 <vishnupv863@gmail.com>
2025-07-18 11:58:19 +0530

    main urls.py updated

--- backend/realestate/urls.py
+++ backend/realestate/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("properties.urls")),
-    path("api/auth/", include("accounts.urls")),
+    path("auth/", include("accounts.urls")),
 ]
