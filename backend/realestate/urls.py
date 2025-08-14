ed7c3dbd9523487fe0b6f55907c4de11469045b4
vishnupv863 <vishnupv863@gmail.com>
2025-08-14 19:58:07 +0530

    error in urls.py has changed

--- backend/realestate/urls.py
+++ backend/realestate/urls.py
@@ -20,8 +20,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from backend import properties
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("accounts.urls")),
