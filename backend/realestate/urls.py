a000f27cb27eda20d33acb166fbac37d392195c1
vishnupv863 <vishnupv863@gmail.com>
2025-08-14 20:12:15 +0530

    urls.py in realestate updated

--- backend/realestate/urls.py
+++ backend/realestate/urls.py
@@ -19,12 +19,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("accounts.urls")),
     path("properties/", include("properties.urls")),
-    path("/", include("properties.urls")),
+    path(
+        "", lambda request: JsonResponse({"message": "Welcome to the Real Estate API!"})
+    ),
 ]
 
 if settings.DEBUG:
